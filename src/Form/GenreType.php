a80a30b002124f1155ceb4a926abc96f5d98e368
BruguierJeremy <jeremybruguier0@gmail.com>
2022-04-04 10:14:41 +0200

    Make CRUD Genre

--- src/Form/GenreType.php
+++ src/Form/GenreType.php
@@ -2,34 +2,28 @@
 
 namespace App\Form;
 
-use App\Entity\Event;
+use App\Entity\Genre;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class EventType extends AbstractType
+class GenreType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name')
-            ->add('description')
-            ->add('date')
-            ->add('price')
             ->add('image')
-            ->add('linkTicketing')
-            ->add('slug')
-            ->add('genres', ChoiceType::class)
-            ->add('region')
-            ->add('user')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('events')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Event::class,
+            'data_class' => Genre::class,
         ]);
     }
 }
