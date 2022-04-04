d3b49c7d2224d8c37ca1c2353eb3fce84ca2476f
BruguierJeremy <jeremybruguier0@gmail.com>
2022-04-04 11:03:56 +0200

    Make CRUD User

---
+++ src/Form/UserType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username')
+            ->add('image')
+            ->add('email')
+            ->add('password')
+            ->add('role')
+            ->add('active')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('regions')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
