8e6e900e6776282f53ed5cb98e8c88cc0e040c6c
viclan33 <vic.lantot2@gmail.com>
2022-04-06 14:10:19 +0200

    CORS - Install dependance nelmio-cors-bundle & configuration nelmio_cors.yaml

--- config/bundles.php
+++ config/bundles.php
@@ -12,4 +12,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
