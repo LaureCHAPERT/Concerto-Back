d4d40886017458471509c419e1aa4ce90dca00b3
viclan33 <vic.lantot2@gmail.com>
2022-04-06 11:57:47 +0200

    Authentication - Access control & LoginController & service.yaml updated

---
+++ src/Controller/LoginController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class LoginController extends AbstractController
+{
+    /**
+     * @Route("back/login", name="app_login")
+     */
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+         // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('login/index.html.twig', [
+            'last_username' => $lastUsername,
+            'error'         => $error,
+        ]);
+    }
+
+    /**
+     * Logout
+     * 
+     * @Route("back/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        // Ce code ne sera jamais exécuté
+        // le composant de sécurité va intercepter la requête avant.
+    }
+}
