2c3d82043cb5fad62d07e622d29c2a07a4ee6051
viclan33 <vic.lantot2@gmail.com>
2022-04-07 14:10:07 +0200

    CSRF-Token - Configuration Form-Type & security/framework.yaml + Login form - CSRF Input hidden

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -39,8 +39,17 @@ class EventType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver): void
     {
+        // CSRF documentation for forms
+        // https://symfony.com/doc/5.4/security/csrf.html#csrf-protection-in-symfony-forms
         $resolver->setDefaults([
             'data_class' => Event::class,
+            // enable/disable CSRF protection for this form
+            'csrf_protection' => true,
+            // the name of the hidden HTML field that stores the token
+            'csrf_field_name' => '_token',
+            // an arbitrary string used to generate the value of the token
+            // using a different string for each form improves its security
+            'csrf_token_id'   => 'event_item',
         ]);
     }
 }
